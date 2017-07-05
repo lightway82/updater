b641d844c0757e99a7712debb0f03517190b6e30
lightway82 <lightway82@gmail.com>
2017-07-05 23:16:14 +0300

    JarExecutor  refactoring

--- src/main/java/org/anantacreative/updater/Utilites/JarExecutor.java
+++ src/main/java/org/anantacreative/updater/Utilites/JarExecutor.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class JarExecutor {
 
     private BufferedReader error;
-    private BufferedReader op;
+    private BufferedReader input;
     private int exitVal;
     private String jarFilePath;
     private List<String> args;
@@ -33,45 +33,36 @@ public class JarExecutor {
         actualArgs.addAll(args);
         try {
             final Runtime re = Runtime.getRuntime();
-            //final Process command = re.exec(cmdString, args.toArray(new String[0]));
             final Process command = re.exec(actualArgs.toArray(new String[0]));
-            this.error = new BufferedReader(new InputStreamReader(command.getErrorStream()));
-            this.op = new BufferedReader(new InputStreamReader(command.getInputStream()));
-            // Wait for the application to Finish
+            error = new BufferedReader(new InputStreamReader(command.getErrorStream()));
+            input = new BufferedReader(new InputStreamReader(command.getInputStream()));
+
             command.waitFor();
             this.exitVal = command.exitValue();
             if (this.exitVal != 0) {
-                throw new IOException("Failed to execure jar, " + this.getExecutionLog());
+                throw new IOException("Failed to execute jar, " + this.getExecutionLog());
             }
             if(printLog) getExecutionLog();
 
         } catch (final IOException | InterruptedException e) {
             throw new JarExecutorException(e.getMessage(), e);
+        }catch (Exception e){
+            throw new JarExecutorException(e.getMessage(), e);
+        }finally {
+            try {
+                error.close();
+                input.close();
+            } catch (IOException e) {}
         }
     }
 
-    public String getExecutionLog() {
-        String error = "";
+    public String getExecutionLog() throws IOException {
         String line;
-        try {
-            while((line = this.error.readLine()) != null) {
-                error = error + "\n" + line;
-            }
-        } catch (final IOException e) {
-        }
-        String output = "";
-        try {
-            while((line = this.op.readLine()) != null) {
-                output = output + "\n" + line;
-            }
-        } catch (final IOException e) {
-        }
-        try {
-            this.error.close();
-            this.op.close();
-        } catch (final IOException e) {
-        }
-        return "exitVal: " + this.exitVal + ", error: " + error + ", output: " + output;
+        StringBuilder output = new StringBuilder("exitVal: " + this.exitVal);
+        output.append(", error: ");
+        while((line = error.readLine()) != null)  output.append("\n ").append(line);
+        while((line = input.readLine()) != null) output.append("\n ").append(line);
+        return output.toString();
     }
 
     public static class JarExecutorException extends Exception {
